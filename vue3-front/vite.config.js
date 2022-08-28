c17eef6378ea85a4bd7d54ec408007874c7b00cb
wuyue <wuyueparagon@163.com>
2022-08-28 22:34:38 +0800

    指定软连接

--- vue3-front/vite.config.js
+++ vue3-front/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { join } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  //指定软连接
+  resolve: {
+    alias: {
+      '@': join(__dirname, '/src')
+    }
+  }
 })
