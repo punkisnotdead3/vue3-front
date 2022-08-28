f06d4e31e81081cf3d3e2b5588a704bbd7f4216b
wuyue <wuyueparagon@163.com>
2022-08-28 14:01:24 +0800

    初始化项目结构

--- vue3-front/src/App.vue
+++ vue3-front/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class='bg-slate-600 rounded-2xl p-8 md:flex md:p-0'>
-    <img class='w-24 h-24 rounded-full mx-auto md:w-48 md:h-auto md:rounded-none' src='./images/芒果.jpeg'>
+    <img class='w-24 h-24 rounded-full mx-auto md:w-48 md:h-auto md:rounded-none' src='assets/images/芒果.jpeg'>
     <div class='pt-6 text-center space-y-4 md:p-8 md:text-left'>
       <p class='text-lg font-medium text-zinc-50'>你啊点啊十大十大爱上打算的阿萨德南京啊谁发的花费很多时间风华绝代撒符合大舅顺丰还是多福多寿发的发</p>
       <div class='font-medium'>
