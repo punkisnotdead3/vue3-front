2f7e6b38bab3b3cf4610bc5b37d50844ea90e4dc
wuyue <wuyueparagon@163.com>
2022-09-14 22:09:20 +0800

    使用vite 来配置 dev prd 不同的 api 地址， 注意写法，另外npm install anywhere 很有用 很方便

--- vue3-front/src/utils/request.js
+++ vue3-front/src/utils/request.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+console.log(import.meta.env.VITE_BASE_API)
+
 const service = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_BASE_API,
   timeout: 5000
 })
 
