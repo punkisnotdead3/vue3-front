a3b8ea70a105b17310dd364c5ff48daf8b328d2d
wuyue <wuyueparagon@163.com>
2022-09-15 23:41:40 +0800

    navigation bar 的样式修正 注意 sticky 以及 shrink的用法，横向的bar 效果就出来了

--- vue3-front/src/views/main/components/navigation/mobile/index.vue
+++ vue3-front/src/views/main/components/navigation/mobile/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class='bg-red-900'>
-    <ul>
-      <li v-for="item in data" :key="item.id" >{{item.name}}</li>
+  <div class='bg-white sticky top-0 left-0 z-10  '>
+    <ul class="relative flex overflow-auto p-1 text-xs text-zinc-600">
+      <li v-for="item in data" :key="item.id" class="shrink-0 px-1.5 py-0.5 z-10  duration-200 ">{{item.name}}</li>
     </ul>
 
 
