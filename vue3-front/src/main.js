51f8398ee4c540b38e57b43a4611205a1236fa9c
wuyue <wuyueparagon@163.com>
2022-09-16 21:13:40 +0800

    根据设备的屏幕宽度 来动态设置 rem的基准 font size 值

--- vue3-front/src/main.js
+++ vue3-front/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './styles/index.scss'
 import router from './router/index.js'
+import { useREM } from '@/utils/flexible.js'
 
-
+useREM()
 createApp(App).use(router).mount('#app')
--- vue3-front/src/utils/flexible.js
+++ vue3-front/src/utils/flexible.js
@@ -1,5 +1,5 @@
 import { computed } from 'vue'
-import {PC_DEVICE_WIDTH} from '../constants/index.js'
+import { PC_DEVICE_WIDTH } from '../constants/index.js'
 import { useWindowSize } from '@vueuse/core'
 
 const { width } = useWindowSize()
@@ -14,9 +14,25 @@ export const isMobileTerminal = computed(() => {
 
 // 线上用这种方式判断
 export const isMobileTerminalBetter = computed(() => {
-// App.vue
-// 正则表达式判断设备的格式，可以判断市面上大部分设备，我这里只对 pc端 和 移动端 做了处理，写在了mounted上
+  // App.vue
+  // 正则表达式判断设备的格式，可以判断市面上大部分设备，我这里只对 pc端 和 移动端 做了处理，写在了mounted上
   let flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
   return flag
 })
+
+// 动态指定 rem 基准值 最大为40px
+// 根据用户的屏幕宽度 进行计算
+
+export const useREM = () => {
+  const MAX_FONT_SIZE = 40
+  // 监听html 解析完成的事件
+  document.addEventListener('DOMContentLoaded', () => {
+    const html = document.querySelector('html')
+    let fontSize = window.innerWidth / 10
+    fontSize = fontSize > MAX_FONT_SIZE ? MAX_FONT_SIZE : fontSize
+    console.log(fontSize + " hehe")
+    html.style.fontSize = fontSize + 'px'
+  })
+}
+
 
