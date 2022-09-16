1b7dafff4b74e7f0d48a278fd5753496bfb7be25
wuyue <wuyueparagon@163.com>
2022-09-16 21:18:24 +0800

    修改 tailwind的 textsize 基准值

--- vue3-front/tailwind.config.cjs
+++ vue3-front/tailwind.config.cjs
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // tailwind 应用范围
-  content: ['./index.html','./src/**/*.{vue,js}'],
+  content: ['./index.html', './src/**/*.{vue,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        xs: ['0.25rem', '0.35rem'],
+        sm: ['0.35rem', '0.45rem'],
+        base: ['0.45rem', '0.55rem'],
+        lg: ['0.55rem', '0.65rem'],
+        xl: ['0.65rem', '0.75rem']
+      }
+    },
   },
   plugins: [],
 }
