ba9c0571373a1e017ea954bef7fd2035df6811bc
wuyue <wuyueparagon@163.com>
2022-09-23 22:05:27 +0800

    组件自动化注册

--- vue3-front/src/libs/index.js
+++ vue3-front/src/libs/index.js
@@ -1,11 +1,16 @@
-import svgIcon from './svg-icon/index.vue'
-import popup from './popup/index.vue'
+
+import { defineAsyncComponent } from 'vue'
 //对外导出svg icon 这个组件
 export default {
     // main.js createapp 中返回的实例
-    install (app){
-        app.component('m-svg-icon',svgIcon),   
-        app.component('m-popup', popup)   
-
+    install(app) {
+        // app.component('m-svg-icon', svgIcon),
+        //     app.component('m-popup', popup)
+        // 这个是vite的特性
+        const components = import.meta.glob('./*/index.vue')
+        for (const [fullPath, fn] of Object.entries(components)) {
+            const name = "m-" + fullPath.replace('./', '').split('/')[0]
+            app.component(name, defineAsyncComponent(fn))
+        }
     }
 }
